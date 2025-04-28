3b790c835fb41f5acab7298e76ce3f309d8fee67
Volt-0808 <andreyabid@gmail.com>
2025-04-28 18:12:52 +0300

    Сделал линии

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+var hour_container = document.getElementById("hour_container")
 
+function createHourLine() {
+    for (let i = 0; i < 23; i++) {
+        const line = document.createElement("div")
+        const textHour = document.createElement("p")
+        line.classList.add("line")
+        textHour.classList.add("textHour")
+        hour_container.appendChild(line)
+        line.appendChild(textHour)
+
+        textHour.innerHTML = i
+    }
+  }
+createHourLine()
