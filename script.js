b61e8f4d122724dfd2879acb1b909487b07d6275
Volt-0808 <andreyabid@gmail.com>
2025-04-29 20:12:35 +0300

    добавил разделения по пол часа

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var hour_container = document.getElementById("hour_container")
 
 function createHourLine() {
-    for (let i = 0; i < 23; i++) {
+    for (let i = .5; i < 23; i+=.5) {
         const line = document.createElement("div")
         const textHour = document.createElement("p")
-        line.classList.add("line")
-        textHour.classList.add("textHour")
+        if (i%1){
+          line.classList.add("smallLine")
+          textHour.classList.add("smallTextHour")
+          textHour.innerHTML = i
+        } else{
+          line.classList.add("line")
+          textHour.classList.add("textHour")
+          textHour.innerHTML = i
+        }
         hour_container.appendChild(line)
         line.appendChild(textHour)
-
-        textHour.innerHTML = i+1
     }
   }
 createHourLine()
