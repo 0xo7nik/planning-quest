d164dd854c43f7e499cc6d24d820290fada1e8d2
Volt-0808 <andreyabid@gmail.com>
2025-05-09 11:56:19 +0300

    добавил функцию появления окна создания

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const hour_container = document.getElementById("hour_container")
+const cross = document.getElementById("cross")
+const plus = document.getElementById("plus")
+const create_container = document.getElementById("create_container")
+
 const lines = document.querySelectorAll(".line")
 
 function createInput(el) {
@@ -11,4 +15,16 @@ function createInput(el) {
 function createAllInput() {
     lines.forEach((el) => {createInput(el)})
   }
+function unplug(){
+  create_container.style.display="flex"
+}
+function plug(){
+  create_container.style.display=""
+}
+
+cross.addEventListener("click", () => {unplug()})
+plus.addEventListener("click", () => {plug()})
+
+
+unplug()
 createAllInput()
