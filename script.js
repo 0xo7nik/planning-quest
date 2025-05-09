46b3e742c9f0c4a190545106352a6507d6e5e4a5
Volt-0808 <andreyabid@gmail.com>
2025-05-09 08:36:48 +0300

    блок для ввода задачи

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-var hour_container = document.getElementById("hour_container")
+const hour_container = document.getElementById("hour_container")
+const lines = document.querySelectorAll(".line")
 
-function createHourLine() {
-    for (let i = .5; i < 23; i+=.5) {
-        const line = document.createElement("div")
-        const textHour = document.createElement("p")
-        if (i%1){
-          line.classList.add("smallLine")
-          textHour.classList.add("smallTextHour")
-          textHour.innerHTML = i
-        } else{
-          line.classList.add("line")
-          textHour.classList.add("textHour")
-          textHour.innerHTML = i
-        }
-        hour_container.appendChild(line)
-        line.appendChild(textHour)
-    }
+function createInput(el) {
+    const input = document.createElement("input")
+    input.setAttribute("type", "text")
+    input.setAttribute("placeholder", "Введите текст...")
+    input.classList.add('inputStyle');
+    el.appendChild(input)
+}
+function createAllInput() {
+    lines.forEach((el) => {createInput(el)})
   }
-createHourLine()
+createAllInput()
