c48ff108094d45061ed3765535828c2e03cb1f5d
Volt-0808 <andreyabid@gmail.com>
2025-05-10 13:06:30 +0300

    Добавил функцию создания задачи и ещё по мелочи

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-const hour_container = document.getElementById("hour_container")
+const task_container = document.getElementById("task_container")
 const cross = document.getElementById("cross")
 const plus = document.getElementById("plus")
 const create_container = document.getElementById("create_container")
+const date = document.getElementById('date')
+const boton = document.getElementById('boton')
 
-const lines = document.querySelectorAll(".line")
-
-function createInput(el) {
-    const input = document.createElement("input")
-    input.setAttribute("type", "text")
-    input.setAttribute("placeholder", "Введите текст...")
-    input.classList.add('inputStyle');
-    el.appendChild(input)
+function create_task(){
+  const task = document.createElement("div")
+  const task_time = document.createElement("p")
+  const task_title = document.createElement("p")
+  task.classList.add("task")
+  task_time.classList.add("task_time")
+  task_title.classList.add("task_title")
+  task_container.appendChild(task)
+  task.appendChild(task_time)
+  task.appendChild(task_title)
+  task_container.appendChild(plus)
 }
-function createAllInput() {
-    lines.forEach((el) => {createInput(el)})
-  }
+
 function unplug(){
-  create_container.style.display="flex"
+  create_container.style.display="none"
 }
 function plug(){
-  create_container.style.display=""
+  create_container.style.display="flex"
 }
 
+boton.addEventListener("click", () => {unplug()})
 cross.addEventListener("click", () => {unplug()})
 plus.addEventListener("click", () => {plug()})
 
-
+date.valueAsDate = new Date()
 unplug()
-createAllInput()
+create_task()
