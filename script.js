22cd85d174b2693a3b58234ab80e60d1aa0a5daf
Volt-0808 <andreyabid@gmail.com>
2025-05-11 12:35:56 +0300

    Добавил функцию удаления задачи

--- script.js
+++ script.js
@@ -8,26 +8,40 @@ const from = document.getElementById("from")
 const to = document.getElementById("to")
 const title = document.getElementById("title")
 
+var list = {}
+// var list = {date:{name:[start_time, end_time]}}
 
-function create_task(){
-  let start_time_error = from.value.length==5
-  let end_time_error = to.value.length==5
-  let name_error = title.value
-  let time_error = Number(from.value.substring(0, 2))*60+Number(from.value.substring(3, 5))<Number(to.value.substring(0, 2))*60+Number(to.value.substring(3, 5))
+
+function create_task(start_time, end_time, name){
+  let start_time_error = start_time.length==5
+  let end_time_error = end_time.length==5
+  let name_error = name
+  let time_error = Number(start_time.substring(0, 2))*60+Number(start_time.substring(3, 5))<Number(end_time.substring(0, 2))*60+Number(end_time.substring(3, 5))
   if (start_time_error&&end_time_error&&name_error&&time_error){
     const task = document.createElement("div")
     const task_time = document.createElement("p")
     const task_title = document.createElement("p")
+    const task_cross = document.createElement("div")
     task.classList.add("task")
     task_time.classList.add("task_time")
     task_title.classList.add("task_title")
+    task_cross.classList.add("task_cross")
     task_container.appendChild(task)
     task.appendChild(task_time)
     task.appendChild(task_title)
+    task.appendChild(task_cross)
     task_container.appendChild(plus)
-    task_time.innerHTML=`${from.value} - ${to.value}`
-    task_title.innerHTML=title.value
+    task_cross.addEventListener("click", () => {
+      remove_child(task)
+      delete list[date_now][name]
+    })
+    task.addEventListener("mouseover", () => {task_cross.style.display=""})
+    task.addEventListener("mouseout", () => {task_cross.style.display="none"})
+    task_cross.style.display="none"
+    task_time.innerHTML=`${start_time} - ${end_time}`
+    task_title.innerHTML=name
     unplug()
+    list[date_now][name] = [start_time, end_time]
   } else{
     let error = ""
     !(start_time_error)?error+="Придурок введи в поле начало времен хоть что то. ":1
@@ -47,10 +61,28 @@ function unplug(){
 function plug(){
   create_container.style.display="flex"
 }
+function chenge_date(){
+  const all_tasks = document.querySelectorAll(".task")
+  all_tasks.forEach((elem)=>{remove_child(elem)})
+  date_now=date.valueAsDate
+  if (!(date.valueAsDate in list)){
+    list[date_now]={}
+  } else{
+    for (const [key, value] of Object.entries(list[date_now])) {
+      create_task(value[0], value[1], key)
+    }
+  }
+}
+function remove_child(elem){
+  elem.parentNode.removeChild(elem)
+}
 
-boton.addEventListener("click", () => {create_task()})
+date.addEventListener("change", () => {chenge_date()})
+boton.addEventListener("click", () => {create_task(from.value, to.value, title.value)})
 cross.addEventListener("click", () => {unplug()})
 plus.addEventListener("click", () => {plug()})
 
-date.valueAsDate = new Date()
+var date_now = new Date()
+date.valueAsDate = date_now
+chenge_date()
 unplug()
