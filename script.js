c8e8543feb2f38249dd94b665db51fbfae1eac91
Volt-0808 <andreyabid@gmail.com>
2025-05-14 21:00:02 +0300

    Добаввил сортировку исправил баги исправил логическое название ошибки

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const task_container = document.getElementById("task_container")
 const cross = document.getElementById("cross")
 const plus = document.getElementById("plus")
 const create_container = document.getElementById("create_container")
+const description = document.getElementById("description")
 const date = document.getElementById("date")
 const boton = document.getElementById("boton")
 const from = document.getElementById("from")
@@ -9,45 +10,68 @@ const to = document.getElementById("to")
 const title = document.getElementById("title")
 
 var list = {}
-// var list = {date:{name:[start_time, end_time]}}
+// var list = {date:{name:[start_time, end_time, description]}}
+var list2 = []
 
 
-function create_task(start_time, end_time, name){
-  let start_time_error = start_time.length==5
-  let end_time_error = end_time.length==5
-  let name_error = name
-  let time_error = Number(start_time.substring(0, 2))*60+Number(start_time.substring(3, 5))<Number(end_time.substring(0, 2))*60+Number(end_time.substring(3, 5))
-  if (start_time_error&&end_time_error&&name_error&&time_error){
+function create_task(start_time, end_time, name, disc, new_task){
+  let start_time_error = 1
+  let name_error_exist = 1
+  let end_time_error = 1
+  let name_error = 1
+  let time_error = 1
+  let time_error_0 = 1
+  if (new_task){
+    let start_time_error = start_time.length==5
+    let name_error_exist = list[date_now][name]==undefined
+    let end_time_error = end_time.length==5
+    let name_error = name
+    let time_error = Number(start_time.substring(0, 2))*60+Number(start_time.substring(3, 5))<=Number(end_time.substring(0, 2))*60+Number(end_time.substring(3, 5))
+    let time_error_0 = Number(start_time.substring(0, 2))*60+Number(start_time.substring(3, 5))!=Number(end_time.substring(0, 2))*60+Number(end_time.substring(3, 5))
+  }
+  if ((!new_task)||(start_time_error&&end_time_error&&name_error&&time_error&&time_error_0&&name_error_exist)){
     const task = document.createElement("div")
     const task_time = document.createElement("p")
     const task_title = document.createElement("p")
     const task_cross = document.createElement("div")
+    const task_clue = document.createElement("div")
+    let desc = disc!=""
     task.classList.add("task")
     task_time.classList.add("task_time")
     task_title.classList.add("task_title")
     task_cross.classList.add("task_cross")
+    task_clue.classList.add("task_clue")
     task_container.appendChild(task)
     task.appendChild(task_time)
     task.appendChild(task_title)
     task.appendChild(task_cross)
-    task_container.appendChild(plus)
+    task.appendChild(task_clue)
     task_cross.addEventListener("click", () => {
       remove_child(task)
       delete list[date_now][name]
     })
-    task.addEventListener("mouseover", () => {task_cross.style.display=""})
-    task.addEventListener("mouseout", () => {task_cross.style.display="none"})
+    task.addEventListener("mouseover", () => {task_cross.style.display=""; desc?task_clue.style.display="":0})
+    task.addEventListener("mouseout", () => {task_cross.style.display="none"; desc?task_clue.style.display="none":0})
     task_cross.style.display="none"
+    task_clue.style.display="none"
+    task_clue.innerHTML=disc
     task_time.innerHTML=`${start_time} - ${end_time}`
     task_title.innerHTML=name
-    unplug()
-    list[date_now][name] = [start_time, end_time]
+    if (new_task){
+      list[date_now][name] = [start_time, end_time, disc, task]
+      list2.forEach((nam)=>{list[date_now][nam][0]>list[date_now][name][0]?task_container.appendChild(list[date_now][nam][3]):0})
+      list2.push(name)
+      unplug()
+    }
+    task_container.appendChild(plus)
   } else{
     let error = ""
-    !(start_time_error)?error+="Придурок введи в поле начало времен хоть что то. ":1
-    !(end_time_error)?error+="Дурак поле окончания времен должно быть полностью заполнено. ":1
-    !(name_error)?error+="Ушлёпок введи в поле названия задачи хотябы строчку. ":1
-    !(time_error)?error+="У тебя время на задачу в минус ушло. Исправляй. ":1
+    !name_error_exist?error+="Поменяй нозвание, утырок, такое уже есть. ":1
+    !(start_time_error)?error+="Придурок, введи в поле начало времен хоть что-то. ":1
+    !(end_time_error)?error+="Дурак, поле окончания времен должно быть полностью заполнено. ":1
+    !(name_error)?error+="Ушлёпок, введи в поле названия задачи хотябы знак. ":1
+    !(time_error)?error+="У тебя, бестолоч, время на задачу в минус ушло. Исправляй. ":1
+    !(time_error_0)?error+="У тебя на задачу времени не капли уходит, флэш тупой. Исправляй. ":1
     alert(error)
   }
 }
@@ -57,6 +81,7 @@ function unplug(){
   from.value=""
   to.value=""
   title.value=""
+  description.value=""
 }
 function plug(){
   create_container.style.display="flex"
@@ -69,7 +94,7 @@ function chenge_date(){
     list[date_now]={}
   } else{
     for (const [key, value] of Object.entries(list[date_now])) {
-      create_task(value[0], value[1], key)
+      create_task(value[0], value[1], key, value[2], 0)
     }
   }
 }
@@ -78,7 +103,7 @@ function remove_child(elem){
 }
 
 date.addEventListener("change", () => {chenge_date()})
-boton.addEventListener("click", () => {create_task(from.value, to.value, title.value)})
+boton.addEventListener("click", () => {create_task(from.value, to.value, title.value, description.value, 1)})
 cross.addEventListener("click", () => {unplug()})
 plus.addEventListener("click", () => {plug()})
 
